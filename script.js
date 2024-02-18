fb9e43f039f084abf2ee76f2e455927f28ddbcb6
killmi117 <esparda_117@hotmail.com>
2024-02-17 18:04:11 -0700

    API
    se le esta agregando una api de google maps

--- script.js
+++ script.js
@@ -31,44 +31,14 @@ function cerrarModal() {
 
 // Maps
 
-function initMap() {
-  // Crea un mapa en el elemento con id "map"
-  var map = new google.maps.Map(document.getElementById('map'), {
-      center: { lat: -34.397, lng: 150.644 },
-      zoom: 8
+function iniciarMap(){
+  var coord = {lat: 31.5889594 ,lng: -106.3760624};
+  var map = new google.maps.Map(document.getElementById('map'),{
+    zoom: 10,
+    center: coord
   });
-
-  // Obtiene la ubicación actual del usuario si es compatible
-  if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(function (position) {
-          var pos = {
-              lat: position.coords.latitude,
-              lng: position.coords.longitude
-          };
-
-          // Centra el mapa en la ubicación del usuario
-          map.setCenter(pos);
-
-          // Crea un marcador en la ubicación del usuario
-          var marker = new google.maps.Marker({
-              position: pos,
-              map: map
-          });
-      }, function () {
-          // Maneja los errores de geolocalización
-          handleLocationError(true, infoWindow, map.getCenter());
-      });
-  } else {
-      // El navegador no soporta geolocalización
-      handleLocationError(false, infoWindow, map.getCenter());
-  }
-}
-
-function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-  // Maneja los errores de geolocalización
-  infoWindow.setPosition(pos);
-  infoWindow.setContent(browserHasGeolocation ?
-      'Error: El servicio de geolocalización falló.' :
-      'Error: Tu navegador no soporta geolocalización.');
-  infoWindow.open(map);
+  var marker = new google.maps.Marker({
+    position: coord,
+    map: map
+  });
 }
